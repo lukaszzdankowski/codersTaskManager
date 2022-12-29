ebb903674a43e4ec7157a4c130a5b24f95c3e572
Łukasz Zdankowski <lukasz.zdankowski@gmail.com>
2022-12-29 08:57:20 +0100

    added printOptions()

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,7 +1,23 @@
 package pl.coderslab;
 
 public class TaskManager {
+    static final String FILE_NAME = "tasks.csv";
+    static final String[] OPTIONS = {"add", "remove", "list", "exit"};
+
+    static String[][] tasks;
+
     public static void main(String[] args) {
+        printOptions(OPTIONS);
 
     }
+
+    public static void printOptions(String[] tab) {
+        System.out.println(ConsoleColors.BLUE + "Please select an option: "
+                + ConsoleColors.RESET);
+        for (String option : tab) {
+            System.out.println(option);
+        }
+    }
+
+
 }
