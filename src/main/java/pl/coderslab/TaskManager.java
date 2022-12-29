09687aea21568a79875925df973618663c89d62f
Łukasz Zdankowski <lukasz.zdankowski@gmail.com>
2022-12-29 09:38:07 +0100

    added loadDataToTab

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,5 +1,11 @@
 package pl.coderslab;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class TaskManager {
     static final String FILE_NAME = "tasks.csv";
     static final String[] OPTIONS = {"add", "remove", "list", "exit"};
@@ -7,6 +13,7 @@ public class TaskManager {
     static String[][] tasks;
 
     public static void main(String[] args) {
+        tasks = loadDataToTab(FILE_NAME);
         printOptions(OPTIONS);
 
     }
@@ -17,6 +24,28 @@ public class TaskManager {
         for (String option : tab) {
             System.out.println(option);
         }
+    }
+
+    public static String[][] loadDataToTab(String fileName) {
+        Path file = Paths.get(fileName);
+        if (!Files.exists(file)) {
+            System.out.println("File not exist.");
+            System.exit(0);
+        }
+        String[][] tab = null;
+        try {
+            List<String> strings = Files.readAllLines(file);
+            tab = new String[strings.size()][strings.get(0).split(",").length];
+            for (int i = 0; i < strings.size(); i++) {
+                String[] split = strings.get(i).split(",");
+                for (int j = 0; j < split.length; j++) {
+                    tab[i][j] = split[j];
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println("ERROR in readAllLines");
+        }
+        return tab;
     }
 
 
